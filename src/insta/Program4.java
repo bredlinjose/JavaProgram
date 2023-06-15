3dc86622894b154e4b9c75f74e92b330060c82bd
bredlin-jose <bredlinjose.t@testyantra.com>
2023-06-15 12:52:07 +0530

    Program4 is modified in insta

--- src/insta/Program4.java
+++ src/insta/Program4.java
@@ -3,24 +3,7 @@ package insta;
 public class Program4 {
 
 	public static void main(String[] args) {
-		String wordString = "my name is bredlin";
-		char[] arr = wordString.toCharArray();
-		char[] result = new char[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == ' ') {
-				result[i] = ' ';
-			}
-		}
-		int num = arr.length - 1;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != ' ') {
-				if (result[num] == ' ') {
-					num--;
-				}
-				result[num] = arr[i];
-				num--;
-			}
-		}
-		System.out.println(result);
+		System.out.println(args[0]);
+		// java.lang.ArrayIndexOutOfBoundsException
 	}
 }
